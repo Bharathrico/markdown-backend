aa07561f8663ef6d4c7835891af67d0e51732125
bharathrico <bharathganesh26899@gmail.com>
2026-09-21 19:21:22 -0400

    Preparing for vercel

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello boi!'))
+
+// Only listen when running locally
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+}
+
+module.exports = app;
